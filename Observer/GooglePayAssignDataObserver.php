e6d9675f0dfe630fde0fd91daa930dfb8b272ffe
Matthew O'Loughlin <matthew.oloughlin@aligent.com.au>
2020-01-26 08:23:56 +1030

    Separate Google and Apple pay into separate payment methods

---
+++ Observer/GooglePayAssignDataObserver.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PMNTS\Gateway\Observer;
+
+class GooglePayAssignDataObserver extends \Magento\Payment\Observer\AbstractDataAssignObserver
+{
+    /**
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        // TODO: Implement me.
+    }
+}
